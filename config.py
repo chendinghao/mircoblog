00d4c39747e3a94f2b592cfc13bf56b14ee84bd9
chendinghao <171196162@qq.com>
2017-08-18 15:43:19 +0800

    1

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 # -*- coding = UTF-8 -*-
+import os
+
 CSRF_ENABLED = True  
 SECRET_KEY = 'you-will-never-guess'
 
@@ -8,3 +10,11 @@ OPENID_PROVIDERS = [
     { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
     { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
     { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' +os.path.join(basedir, 'app,db') 
+
+
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, "db_repository")
